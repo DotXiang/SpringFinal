6879c1dc7e43dfe4dad74bdd4be40e23421954a7
Michal Ziarniak <pyrazi@o2.pl>
2014-06-24 21:04:31 +0200

    Change game model class

--- FinalProject/src/com/ziarniak/project/models/Game.java
+++ FinalProject/src/com/ziarniak/project/models/Game.java
@@ -10,25 +10,23 @@ import org.springframework.stereotype.Component;
 import com.ziarniak.project.validation.Unique;
 
 @Component
-public class Game implements Serializable{
+public class Game implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	@Unique(value = Game.class, property = "name")
-	@Size(min=1, max=30)
+	@Size(min = 1, max = 30)
 	private String name;
-	@Size(min=1, max=30)
+	@Size(min = 1, max = 30)
 	private String maker;
-	@Size(min=1, max=30)
+	@Size(min = 1, max = 30)
 	private String type;
-	@Digits(integer=3,fraction=2)
+	@Digits(integer = 3, fraction = 2)
 	private String price;
-	
-	
-	public Game(){
+
+	public Game() {
 	}
-	
-	
+
 	public Game(String name, String maker, String type, String price) {
 		super();
 		this.name = name;
@@ -37,45 +35,54 @@ public class Game implements Serializable{
 		this.price = price;
 	}
 
-
-	public String[] getFields(){
-		return new String[]{"id","name","maker","type","price"};
+	public String[] getFields() {
+		return new String[] { "id", "name", "maker", "type", "price" };
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMaker() {
 		return maker;
 	}
+
 	public void setMaker(String maker) {
 		this.maker = maker;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getPrice() {
 		return price;
 	}
+
 	public void setPrice(String price) {
 		this.price = price;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	@Override
 	public String toString() {
 		return "Game [id=" + id + ", name=" + name + ", maker=" + maker
 				+ ", type=" + type + ", price=" + price + "]";
 	}
-	
-	
+
 }
